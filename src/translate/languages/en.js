832b7ca037e4de4743e9cecbf919658997858793
farzac <zaccantte@gmail.com>
2019-09-01 02:46:13 -0300

    second commit

---
+++ src/translate/languages/en.js
@@ -0,0 +1,18 @@
+const messages = {
+    en: {
+        translations: {
+            titles: {
+                app: 'My application'
+            },
+            messages: {
+                Itworks: 'It work perfect',
+                smallText: 'A small text'
+            },
+            buttons: {
+                save: 'Save'
+            },
+        }
+    }
+}
+
+export { messages } 
---
+++ src/translate/languages/pt.js
@@ -0,0 +1,19 @@
+const messages = {
+    pt: {
+        translations : {
+            titles: {
+                app : 'Minha aplicação'
+            },
+            messages : {
+                Itworks : 'Esta funcionando com varias idiomas',
+                smallText: 'Pequena parte do funcao' 
+            
+            },
+            buttons: {
+                save: 'Salvar' 
+            },
+        }
+    }
+}
+
+export { messages }
